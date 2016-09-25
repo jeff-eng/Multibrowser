59dccf7e5e9465f977fc84c2c471ab7c41c1724c
Jeff Eng <jeff@jeffeng.me>
2016-09-25 15:51:39 -0700

    set up the interface with the stack view and text field

---
+++ Project31/Project31/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by Jeffrey Eng on 9/24/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var addressBar: UITextField!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
