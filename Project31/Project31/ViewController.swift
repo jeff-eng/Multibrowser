f70361a38e5bb4b028fb7145741b7ae403c01c00
Jeff Eng <jeff@jeffeng.me>
2016-09-26 14:56:38 -0700

    added method to set active web view with border color highlight, tap gesture recognizer methods for when web view tapped, and textFieldShouldReturn method to load url when pressing return key

--- Project31/Project31/ViewController.swift
+++ Project31/Project31/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate, 
     
     @IBOutlet weak var stackView: UIStackView!
     
+    weak var activeWebView: UIWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,17 +41,61 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate, 
         let url = NSURL(string: "https://www.hackingwithswift.com")!
         
         webView.loadRequest(NSURLRequest(URL: url))
+        
+        
+        webView.layer.borderColor = UIColor.blueColor().CGColor
+        selectWebView(webView)
+        
+        // lets our tap gesture recognizer and built-in ones work at the same time
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+    }
+    
+    func selectWebView(webView: UIWebView) {
+        // loop through the array of web views belonging to the stack view, updating them to have a zero-width border line, then set the newly selected one to have a border width of three pts.
+        
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        
+        // set the active web view property to the newly selected web view
+        activeWebView = webView
+        // update the border width to 3 pts
+        webView.layer.borderWidth = 3
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let webView = activeWebView, let address = addressBar.text {
+            if let url = NSURL(string: address) {
+                webView.loadRequest(NSURLRequest(URL: url))
+            }
+        }
+        
+        textField.resignFirstResponder()
+        return true
     }
     
     func deleteWebView() {
         
     }
 
+    func webViewTapped(recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? UIWebView {
+            selectWebView(selectedWebView)
+        }
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
