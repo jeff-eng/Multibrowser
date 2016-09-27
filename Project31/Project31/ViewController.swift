a58a25bf4d468ec43bb8f2ad668dc8c146b45332
Jeff Eng <jeff@jeffeng.me>
2016-09-27 16:08:55 -0700

    added the method that detects compact size class and applies vertical stacking, otherwise if regular, then use horizontal stacking

--- Project31/Project31/ViewController.swift
+++ Project31/Project31/ViewController.swift
@@ -119,6 +119,14 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate, 
         return true
     }
     
+    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+        if traitCollection.horizontalSizeClass == .Compact {
+            stackView.axis = .Vertical
+        } else {
+            stackView.axis = .Horizontal
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
